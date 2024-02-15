b23e11bb38df6709978403d6c7beb8e4376585f6
meninodevelopment <144365071+meninodevelopment@users.noreply.github.com>
2024-02-15 13:09:35 -0300

    Create aula_01.py

---
+++ modulo_01/aula_01.py
@@ -0,0 +1,7 @@
+#isso é um comentário em python
+
+"""
+    isso não é um comentário, por mais que não dê erro e não faça nada. O nome disso é "DocString"
+"""
+
+print("Hello World!") #funcão que imprime algo no console (output)
